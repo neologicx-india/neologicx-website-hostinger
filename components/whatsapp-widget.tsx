37f774c740005260cefcaeef8c2ed175f90eb507
Pradhuman <pradhuman.sharma@neologicx.in>
2026-09-22 16:52:31 +0530

    rggR

--- components/whatsapp-widget.tsx
+++ components/whatsapp-widget.tsx
@@ -13,7 +13,7 @@ export default function WhatsAppWidget() {
   const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(chatMessage)}`;
 
   return (
-    <div className="fixed bottom-24 right-6 z-50 flex flex-col items-end">
+    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end">
       <AnimatePresence>
         {isOpen && (
           <motion.div
